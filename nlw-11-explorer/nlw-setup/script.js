cf826b1d5b1dfc0c65727edee237b7db14ca2b1c
andreclourenco <andre.cura.lourenco@gmail.com>
2023-01-20 15:40:53 +0000

    update/nlw-11-explorer-class04

--- nlw-11-explorer/nlw-setup/script.js
+++ nlw-11-explorer/nlw-setup/script.js
@@ -1,11 +1,29 @@
 const form = document.querySelector("#form-habits");
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08", "01-09"],
-  takePills: ["01-03"],
-  journal: ["01-02"],
-};
+button.addEventListener("click", add);
+form.addEventListener("change", save);
 
+function add() {
+  const today = new Date().toLocaleDateString("pt-PT").slice(0, 5);
+
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    alert("Day Already Exists ❌");
+    return;
+  }
+
+  alert("Day added ✔️");
+  nlwSetup.addDay("20/01");
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@Habits", JSON.stringify(nlwSetup.data));
+}
+
+// se objecto existir, carrega as informações, se não, cria objeto vazio
+const data = JSON.parse(localStorage.getItem("NLWSetup@Habits")) || {};
 nlwSetup.setData(data);
 nlwSetup.load();
